abd695f53f40341d92ab924e9b21970d2b31188a
Taha <taha.othman@outlook.com>
2023-03-08 13:46:50 -0500

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    name: "name",
+    streetAddress: "",
+}
+
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObject = { ...employee};
+    newObject[key] = value;
+    
+    return newObject;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const deletedObject = {...employee};
+
+    delete deletedObject[key];
+    return deletedObject;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+
+    
+
+}
